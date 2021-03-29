5078c2044cec2becf44e974a3af0187d6616263c
Alex <alexbachurin91@gmail.com>
2021-03-29 04:23:31 +0300

    added delete button

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+// import {evaluate} from 'mathjs';
+
+
 window.addEventListener('DOMContentLoaded', () => {
     const btns = document.querySelectorAll('.calc__button-digit');
     const operators = document.querySelectorAll('.calc__button-operation'),
@@ -10,33 +13,35 @@ window.addEventListener('DOMContentLoaded', () => {
     let result = 0; //set default result
     let currentVal = '0'; //set default input value
     let isSignEquallyPressed = false; //helping variable to see if we clicked on "="
-    output.textContent = currentVal;
+    let canDelete = false; //helping variable to see if we can delete or not
+    output.textContent = '0';
     btns.forEach(btn => {
         btn.addEventListener('click', (e) => {
             e.preventDefault();
             if (e.target) {
 
-                // some trick, if we have pressed "=" our currentVal now equals that result and its type is 'number' , so we check it, and if it is number we reset value back to empty string
-                //so we can imediatly rewrite calculator output and start new calculations
-                // if (typeof currentVal === 'number') {
-                //     console.log('NUMBER')
-                //     currentVal = '';
-                // }
+                //reset value to empty string if we pressed "=" , so when we start pressing on buttons we can calculate new expression
                 if (isSignEquallyPressed) {
                     currentVal = '';
                     isSignEquallyPressed = false;
                 }
-                
-                
+
+                //check if can delete in false state, if so set it to true, so we can delete numbers 
+                if (!canDelete) {
+                    canDelete = true;
+                }
+
+
+
+
                 //check our value on 0 in the beginning so we cant write something like this : 00000003
                 //but we want to write numbers like so : 0.03 , so we check if we pressed on dot('.') button
-                if (currentVal.startsWith('0') && !currentVal.includes('.')) {
+                if (currentVal.toString().startsWith('0') && !currentVal.toString().includes('.')) {
                     console.log('true')
                     currentVal = '';
                     console.log(currentVal)
                 }
 
-                
                 //when we click on button write it text content to input value
                 currentVal += btn.textContent;
                 //check if we have more than 1 value in array, if yes then 
@@ -50,6 +55,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     output.textContent = currentVal;
                     // console.log(output.textContent)
                 }
+                
 
                 // console.log(currentVal)
             }
@@ -60,9 +66,8 @@ window.addEventListener('DOMContentLoaded', () => {
     //so we get array like this ['1', '+'], then we check if array contains 2 values
     //if yes, then on next operator click we add next value in array so it now 
     //looks like this ['1', '+', '3'] and evaluate result, clear array and put result as first value 
-    //in array, so we getting array with result: arr = [result],
+    //in array, so we getting array with result: arr = [4],
     //so in next steps we just adding pressed operator and calculating new value based on prev result;
-    //with help 
     operators.forEach(oper => {
         oper.addEventListener('click', (e) => {
             //check if value is not an empty string
@@ -78,7 +83,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 //check if there is 2 values in array
                 //if yes then calculate result, clear array, and push result in array
                 if (arr.length > 1) {
-                    result = eval(arr.join(''));
+                    result = math.evaluate(arr.join(''));
                     arr = [];
                     arr.push(result);
                     output.textContent = result;
@@ -125,8 +130,22 @@ window.addEventListener('DOMContentLoaded', () => {
             //add zeros if we pressed "." with empty value
             if (currentVal === '') {
                 currentVal += `0${dot.textContent}`;
+                if (arr.length > 1) {
+                    topOutput.textContent = arr.join('') + currentVal;
+                    output.textContent = currentVal;
+                } else {
+                    topOutput.textContent = currentVal;
+                    output.textContent = currentVal;
+                }
             } else {
                 currentVal += dot.textContent;
+                if (arr.length > 1) {
+                    topOutput.textContent = arr.join('') + currentVal;
+                    output.textContent = currentVal;
+                } else {
+                    topOutput.textContent = currentVal;
+                    output.textContent = currentVal;
+                }
             }
 
         }
@@ -144,7 +163,7 @@ window.addEventListener('DOMContentLoaded', () => {
             if (arr.length > 1) {
                 isSignEquallyPressed = true;
                 arr.push(currentVal);
-                result = eval(arr.join(''));
+                result = math.evaluate(arr.join(''))
                 arr = [];
                 // arr.push(result);
                 output.textContent = result;
@@ -154,6 +173,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 console.log(result);
                 console.log(arr);
                 console.log(typeof currentVal)
+                //we cant delete result
+                canDelete = false;
             }
         }
         // console.log(currentVal);
@@ -190,6 +211,8 @@ window.addEventListener('DOMContentLoaded', () => {
     square.addEventListener('click', () => {
         if (currentVal) {
             currentVal *= currentVal;
+            //canDelete need to be setted in false for right work
+            canDelete = false;
             if (arr.length > 1) {
                 topOutput.textContent = arr.join('') + currentVal;
                 output.textContent = currentVal;
@@ -197,7 +220,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 topOutput.textContent = currentVal;
                 output.textContent = currentVal;
             }
-            
+
         }
     })
 
@@ -218,10 +241,52 @@ window.addEventListener('DOMContentLoaded', () => {
                     topOutput.textContent = currentVal;
                     output.textContent = currentVal;
                 }
-            } else  {
+            } else {
                 currentVal = Math.abs(currentVal);
                 if (arr.length > 1) {
                     topOutput.textContent = arr.join('') + currentVal;
+                    output.textContent = currentVal;
+                } else {
+                    topOutput.textContent = currentVal;
+                    output.textContent = currentVal;
+                }
+                console.log(currentVal)
+            }
+        }
+    })
+
+    // delete
+    const deleteBtn = document.querySelector('.calc__button-delete');
+
+    deleteBtn.addEventListener('click', () => {
+        //check if we have not an empty value and we can delete
+        if (currentVal && canDelete) {
+            console.log('click')
+            //check if we have number in currentValue then transform it to string
+            if (typeof currentVal === 'number') {
+                currentVal = currentVal.toString();
+                console.log('number')
+            }
+            //check if we have more then 1 number in currentValue input
+            //if yes - delete one char, if not - set currentValue to '0', also check if we have string like this '-8' so if we press on delete, it sets value '0' not to '-'
+            if (currentVal.length === 1 || (currentVal.length == 2 && currentVal.startsWith('-'))) {
+                console.log('length = 1')
+                currentVal = '0';
+                if (arr.length > 1) {
+                    topOutput.textContent = arr.join('') + currentVal;
+                    // console.log(output.textContent);
+                    output.textContent = currentVal;
+                } else {
+                    topOutput.textContent = currentVal;
+                    output.textContent = currentVal;
+                }
+                console.log(currentVal)
+            } else {
+                console.log('length > 1')
+                currentVal = currentVal.slice(0, currentVal.length - 1);
+                if (arr.length > 1) {
+                    topOutput.textContent = arr.join('') + currentVal;
+                    // console.log(output.textContent);
                     output.textContent = currentVal;
                 } else {
                     topOutput.textContent = currentVal;
